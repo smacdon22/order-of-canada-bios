f429ddfa3f8aeeb2bb7c3a9bdff3aa1190f91592
sarah:) <89022489+smacdon22@users.noreply.github.com>
2023-02-04 13:51:10 -0400

    Add files via upload

---
+++ merge_data.py
@@ -0,0 +1,33 @@
+import csv
+
+# getting data from files
+order_info = open("20220419-1967-2021-OrderOfCanada.csv", encoding='mac_roman')
+order_bios = open("04012023-bio.xlsx - Sheet1.csv")
+
+info = [x for x in csv.reader(order_info)]
+bios = [x for x in csv.reader(order_bios)]
+
+order_info.close()
+order_bios.close()
+
+# just some index and field checks
+c = 0
+for i in info[0]:
+    print(c, i, bios[0].index(i) if i in bios[0] else -1)
+    c += 1
+
+c = 0
+for i in bios[0]:
+    print(c, i, info[0].index(i) if i in info[0] else -1)
+    c += 1
+
+# rewrite the rows with award info as well
+new_csv_file = open("merged_order_of_canada_data.csv", "w")
+csv_writer = csv.writer(new_csv_file)
+
+for i in range(8014):
+    all_info = [info[0], info[1], info[2], info[3], info[4], info[5], info[6], info[7], info[8], info[9], info[10], info[11], info[12], info[13], info[14], info[15], info[16], info[17], info[18], info[19], info[20], info[21], info[22], info[23], info[24], info[25], info[26], info[27], info[28], info[29], bios[7], bios[8], bios[9], bios[10], bios[11], bios[12], bios[13], bios[14], bios[15], bios[16], bios[17], bios[18], bios[19], bios[20], bios[21], bios[22], bios[23]]
+    csv_writer.writerow([info[i][0], info[i][1], info[i][2], info[i][3], info[i][4], info[i][5], info[i][6], info[i][7], info[i][8], info[i][9], info[i][10], info[i][11], info[i][12], info[i][13], info[i][14], info[i][15], info[i][16], info[i][17], info[i][18], info[i][19], info[i][20], info[i][21], info[i][22], info[i][23], info[i][24], info[i][25], info[i][26], info[i][27], info[i][28], info[i][29], bios[i][7], bios[i][8], bios[i][9], bios[i][10], bios[i][11], bios[i][12], bios[i][13], bios[i][14], bios[i][15], bios[i][16], bios[i][17], bios[i][18], bios[i][19], bios[i][20], bios[i][21], bios[i][22], bios[i][23]])
+
+new_csv_file.close()
+
